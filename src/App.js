43b9c88634e58db01d81e3553f7cbad022b230b0
NBO2001 <natanbezerr63@gmail.com>
2021-07-15 16:06:02 -0400

    Skelleton project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from "react";
+import { ThemeProvider } from "styled-components";
+import { Reset } from 'styled-reset';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+import theme from "./theme";
+import Home from "./pages/Home";
 
 const App = () => {
 
   return(
-    <h1>Hello</h1>
+    <ThemeProvider theme={theme}>
+      <Reset />
+      <Router>
+        <Switch>
+          <Route path='/home'>
+              <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 
 }
